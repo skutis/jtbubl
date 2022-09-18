608171575b6230dd26624012bdfc94768bdc28fc
Jose Tejada <jose.tejada@jotego.es>
2022-09-18 13:17:11 +0200

    exterm: sound module added

---
+++ cores/exterm/hdl/jtexterm_colmix.v
@@ -0,0 +1,53 @@
+/*  This file is part of JTBUBL.
+    JTBUBL program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JTBUBL program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JTBUBL.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Jose Tejada Gomez. Twitter: @topapate
+    Version: 1.0
+    Date: 18-9-2022 */
+
+module jtexterm_colmix(
+    input        clk,
+    input        clk_cpu,
+    input        pxl_cen,
+    input        LHBL,
+    input        LVBL,
+    input  [9:0] cpu_addr,
+    input  [7:0] cpu_dout,
+    input        cpu_wrn,
+    output [7:0] cpu_din,
+
+    output [4:0] red,
+    output [4:0] green,
+    output [4:0] blue,
+
+    input  [3:0] gfx_en,
+);
+
+// Palette RAM X1-007 chip
+jtframe_dual_ram #(.aw(10)) u_comm(
+    .clk0   ( clk_cpu            ),
+    .clk1   ( clk24              ),
+    // Main CPU
+    .addr0  ( cpu_addr           ),
+    .data0  ( cpu_dout           ),
+    .we0    ( ~cpu_wrn           ),
+    .q0     ( cpu_din            ),
+    // Color mixer
+    .addr1  ( pal_addr           ),
+    .data1  (                    ),
+    .we1    ( 1'b0               ),
+    .q1     ( pal_dout           )
+);
+
+endmodule
--- cores/exterm/hdl/jtexterm_main.v
+++ cores/exterm/hdl/jtexterm_main.v
@@ -22,31 +22,28 @@ module jtexterm_main(
     input               cen6,
     input               LVBL,
 
-    // Cabinet inputs
-    input      [ 1:0]   start_button,
-    input      [ 1:0]   coin_input,
-    input      [ 5:0]   joystick1,
-    input      [ 5:0]   joystick2,
-
-    // Sound interface
-    output reg          snd_rstn,
-
     // Video devices
     output reg          seta_cs,
     output reg          pal_cs,
     output     [ 7:0]   cpu_dout,
     output              cpu_wrn,
 
-    // Main CPU ROM interface
-    output     [17:0]   rom_addr,
+    // ROM interface
+    output reg [16:0]   rom_addr,
     output reg          rom_cs,
     input               rom_ok,
     input      [ 7:0]   rom_data,
 
+    // Sub CPU (sound)
+    output reg          snd_rstn,
+    //      access to RAM
+    input      [12:0]   sub_addr,
+    input      [ 7:0]   sub_dout,
+    input               sub_we,
+    output     [ 7:0]   sub_din,
+
     // DIP switches
-    input               dip_pause,
-    input               service,
-    input      [15:0]   dipsw
+    input               dip_pause
 );
 
 wire        irq_ack, mreq_n, m1_n, iorq_n, rd_n, wr_n;
@@ -59,11 +56,21 @@ assign cpu_wrn = wr_n;
 assign irq_ack = /*!m1_n &&*/ !iorq_n; // The original PCB just uses iorq_n,
     // the orthodox way to do it is to use m1_n too
 
+always @* begin
+    rom_addr = { 2'b0, A[14:0] };
+    if( A[15] ) begin
+        rom_addr[16:14] = 3'b1 + bank;
+    end
+end
+
 always @(posedge clk) begin
-    rom_cs  <= !mreq_n && A[15:12]<4'hc;
-    ram_cs  <= !mreq_n && A[15:12]==4'he;
-    bank_cs <= !mreq_n && A[15: 8]==8'hf6;
-    pal_cs  <= !mreq_n && A[15: 8]==8'hf8;
+    rom_cs      <= !mreq_n && A[15:12]  < 4'hc;
+    vram_cs     <= !mreq_n && A[15:13] == 3'b110; // C,D
+    ram_cs      <= !mreq_n && A[15:12] == 4'he; // A[12:0] used in Insector X (?)
+    gfx_ctrl_cs <= !mreq_n && A[15: 8] == 8'hf3;
+    gfx_lyr_cs  <= !mreq_n && A[15: 8] == 8'hf4;
+    bank_cs     <= !mreq_n && A[15: 8] == 8'hf6;
+    pal_cs      <= !mreq_n && A[15: 8] == 8'hf8;
 end
 
 always @(posedge clk, posedge rst) begin
@@ -71,8 +78,10 @@ always @(posedge clk, posedge rst) begin
         bank     <= 0;
         snd_rstn <= 0;
     end else begin
-        if( bank_cs ) bank <= cpu_dout[2:0];
-        snd_rstn <= cpu_dout[4];
+        if( bank_cs ) begin
+            bank <= cpu_dout[2:0];
+            snd_rstn <= cpu_dout[4];
+        end
     end
 end
 
@@ -83,11 +92,10 @@ jtframe_ff u_irq(
     .din    ( 1'b1      ),
     .q      (           ),
     .qn     ( int_n     ),
-    .set    ( 1'b0      ),    // active high
-    .clr    ( irq_ack   ),    // active high
-    .sigedge( ~LVBL     ) // signal whose edge will trigger the FF
+    .set    ( 1'b0      ),
+    .clr    ( irq_ack   ),
+    .sigedge( ~LVBL     )
 );
-
 
 jtframe_z80_devwait u_gamecpu(
     .rst_n    ( ~rst           ),
@@ -113,20 +121,21 @@ jtframe_z80_devwait u_gamecpu(
     .dev_busy ( 1'b0           )
 );
 
-// Time shared
-jtframe_dual_ram #(.aw(10)) u_comm(
+// first come, first served
+// TODO: add bus contention
+jtframe_dual_ram #(.aw(12)) u_comm(
     .clk0   ( clk24              ),
     .clk1   ( clk24              ),
     // Main CPU
-    .addr0  ( main_addr[9:0]     ),
-    .data0  ( main_dout          ),
-    .we0    ( mcram_we           ),
-    .q0     ( comm2main          ),
+    .addr0  ( A[12:0]            ),
+    .data0  ( cpu_dout           ),
+    .we0    ( ram_we             ),
+    .q0     ( ram_dout           ),
     // MCU
-    .addr1  ( mcu_bus[9:0]       ),
-    .data1  ( rammcu_din         ),
-    .we1    ( rammcu_we          ),
-    .q1     ( comm2mcu           )
+    .addr1  ( sub_addr           ),
+    .data1  ( sub_dout           ),
+    .we1    ( sub_we             ),
+    .q1     ( sub_din            )
 );
 
 
