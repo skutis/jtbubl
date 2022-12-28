f4a29008a76faa91ebd297b8cb14fe65ee31e989
Jose Tejada <jotego@gmail.com>
2022-12-28 20:32:22 +0100

    wip

--- rom/cp_kiwi.sh
+++ rom/cp_kiwi.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-cp chukatai.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp chukaaij.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp chukaija.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp chukaaiu.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp drtoppel.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp drtopelj.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp drtopelu.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp extrmatn.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp extrmtnj.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp extrmtnu.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp extrmnur.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp insectx.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp insectxj.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp kabukiz.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp kabukizj.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp kageki.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp kagekih.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp kagekij.rom /media/$USER/POCKET/Assets/jtkiwi/common
 cp kagekiu.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp plumppop.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp tnzsjo.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp tnzso.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp tnzsoa.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp tnzsop.rom /media/$USER/POCKET/Assets/jtkiwi/common
-cp tnzsuo.rom /media/$USER/POCKET/Assets/jtkiwi/common
